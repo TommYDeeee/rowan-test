a5c279a9b0dfb1bd91cb7cd8df821bdd29ecfe62
Tomáš Ďuriš <Tomas.Duris@gendigital.com>
2023-12-20 09:27:17 +0100

    fix import in doctest

--- src/green/builder.rs
+++ src/green/builder.rs
@@ -61,7 +61,7 @@ impl GreenNodeBuilder<'_> {
     /// `start_node_at`.
     /// Example:
     /// ```rust
-    /// # use rowan::{GreenNodeBuilder, SyntaxKind};
+    /// # use crate::{GreenNodeBuilder, SyntaxKind};
     /// # const PLUS: SyntaxKind = SyntaxKind(0);
     /// # const OPERATION: SyntaxKind = SyntaxKind(1);
     /// # struct Parser;
